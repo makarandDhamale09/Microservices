6a122cd3622639c55603387f7bf5305a38165337
makaranddhamale <makarand.dhamale09@gmail.com>
2023-08-31 23:34:31 +0530

    Added JWT service implementation

---
+++ security-jwt/src/main/java/com/myauth/security/config/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package com.myauth.security.config;
+
+import com.myauth.security.user.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+public class ApplicationConfig {
+
+  private final UserRepository userRepository;
+
+  public ApplicationConfig(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return username ->
+        userRepository
+            .findByEmail(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+  }
+}
---
+++ security-jwt/src/main/java/com/myauth/security/config/JwtService.java
@@ -0,0 +1,70 @@
+package com.myauth.security.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+  private static final String SECRET_KEY =
+      "71fa4bcc42d3dc482b1ed39d1f6bef441171c42a20e6569b2050a2cf0ac508ea";
+
+  public String extractUserName(String jwt) {
+    return extractClaim(jwt, Claims::getSubject);
+  }
+
+  public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = extractALlClaims(token);
+    return claimsResolver.apply(claims);
+  }
+
+  public String generateToken(UserDetails userDetails) {
+    return generateToken(new HashMap<>(), userDetails);
+  }
+
+  public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+    return Jwts.builder()
+        .setClaims(extraClaims)
+        .setSubject(userDetails.getUsername())
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+        .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+        .compact();
+  }
+
+  public boolean isTokenValid(String token, UserDetails userDetails) {
+    final String userName = extractUserName(token);
+    return userName.equals(userDetails.getUsername()) && !isTokenExpired(token);
+  }
+
+  private boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
+  }
+
+  private Date extractExpiration(String token) {
+    return extractClaim(token, Claims::getExpiration);
+  }
+
+  private Claims extractALlClaims(String token) {
+    return Jwts.parserBuilder()
+        .setSigningKey(getSignInKey())
+        .build()
+        .parseClaimsJws(token)
+        .getBody();
+  }
+
+  private Key getSignInKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+    return Keys.hmacShaKeyFor(keyBytes);
+  }
+}
