130ef07d1fafc3076af4d554f7b4b2ae064b5de6
makaranddhamale <makarand.dhamale09@gmail.com>
2024-01-21 23:23:08 +0530

    Postgres connection added

--- kotlin/employee-service/src/main/kotlin/com/myproject/domain/Employee.kt
+++ kotlin/employee-service/src/main/kotlin/com/myproject/domain/Employee.kt
@@ -1,10 +1,10 @@
 package com.myproject.domain
 
 import jakarta.persistence.Entity
+import jakarta.persistence.Id
 import jakarta.persistence.Table
 import java.util.Date
 import java.util.UUID
-
 
 @Entity
 @Table
@@ -14,5 +14,6 @@ class Employee(
     var salary: Long = 0,
     var department: String = "",
     val dateOfBirth: Date = Date(),
+    @Id
     val id: UUID = UUID.randomUUID()
 )
