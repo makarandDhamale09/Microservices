7054c1bb84a482b48f871fc5762a49d4e08f919c
Makarand Dhamale <makarand.dhamale09@gmail.com>
2024-01-23 16:23:58 +0530

    Added hazelcast configuration

--- kotlin/employee-service/src/main/kotlin/com/myproject/EmployeeServiceApplication.kt
+++ kotlin/employee-service/src/main/kotlin/com/myproject/EmployeeServiceApplication.kt
@@ -1,5 +1,7 @@
 package com.myproject
 
+import com.hazelcast.core.Hazelcast
+import com.hazelcast.map.IMap
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +10,9 @@ class EmployeeServiceApplication
 
 fun main(args: Array<String>) {
     runApplication<EmployeeServiceApplication>(*args)
+    val hz = Hazelcast.newHazelcastInstance()
+    val map: IMap<String, String> = hz.getMap("testMap")
+
+    map.set("test", "test")
+    map.set("test2", "test2")
 }
