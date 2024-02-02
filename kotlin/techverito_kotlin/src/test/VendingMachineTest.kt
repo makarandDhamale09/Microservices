6b5a8014c9bdd25b22567eca6ea2d86f0d4dc7c1
makaranddhamale <makarand.dhamale09@gmail.com>
2024-02-02 22:39:36 +0530

    Added partial testcases

--- kotlin/techverito_kotlin/src/test/VendingMachineTest.kt
+++ kotlin/techverito_kotlin/src/test/VendingMachineTest.kt
@@ -1,13 +1,18 @@
+import Type.*
 import org.junit.Before
 import org.junit.Test
 
 class VendingMachineTest {
+    private var vending = VendingMachine()
 
     @Before
     fun setUp() {
+        val products = mutableListOf(Product(CHIPS, 10), Product(COKE, 15), Product(COOKIES, 20))
+        products.forEach { vending.addProducts(it) }
     }
 
     @Test
     fun addProducts() {
+        //assertEquals(3, v)
     }
 }
